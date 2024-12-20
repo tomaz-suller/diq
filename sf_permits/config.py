245435e664f04614a225eaaaf8c29a5c1c2a769f
tomaz-suller <tomaz.suller@usp.br>
2024-12-20 08:34:22 +0100

    Add interim data directory

--- sf_permits/config.py
+++ sf_permits/config.py
@@ -14,19 +14,24 @@ DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
-RAW_DATASET_FILENAME = "building_permits.csv"
-RAW_DATASET_PATH = RAW_DATA_DIR / RAW_DATASET_FILENAME
-PROFILING_DATA_DIR = DATA_DIR / "profiling"
-CLEAN_DATA_DIR = DATA_DIR / "clean"
-CLEAN_DATASET_PATH = CLEAN_DATA_DIR / "dataset.parquet"
-NEIGHBOURHOOD_SHAPEFILE_PATH = EXTERNAL_DATA_DIR  / "analysis-neighborhoods"
-ZIP_CODE_SHAPEFILE_PATH = EXTERNAL_DATA_DIR / "bay-area-zip-codes"
-STREET_NAMES_PATH = EXTERNAL_DATA_DIR / "street-names.csv"
-
 MODELS_DIR = PROJ_ROOT / "models"
 
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
+
+# Project specific paths
+PROFILING_DATA_DIR = DATA_DIR / "profiling"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+CLEAN_DATA_DIR = DATA_DIR / "clean"
+
+NEIGHBOURHOOD_SHAPEFILE_PATH = EXTERNAL_DATA_DIR  / "analysis-neighborhoods"
+ZIP_CODE_SHAPEFILE_PATH = EXTERNAL_DATA_DIR / "bay-area-zip-codes"
+STREET_NAMES_PATH = EXTERNAL_DATA_DIR / "street-names.csv"
+RAW_DATASET_FILENAME = "building_permits.csv"
+RAW_DATASET_PATH = RAW_DATA_DIR / RAW_DATASET_FILENAME
+CLEAN_DATASET_PATH = CLEAN_DATA_DIR / "dataset.parquet"
+INTERIM_DATASET_PATH = INTERIM_DATA_DIR / "dataset.parquet"
+
 
 # If tqdm is installed, configure loguru with tqdm.write
 # https://github.com/Delgan/loguru/issues/135
